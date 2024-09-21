dbfb520fc5c74f46ff6b1443f94e608f2d3f701d
tisonpatrik <patriktison@gmail.com>
2024-09-21 18:49:53 +0200

    first functional version of grcp server

--- calculator/src/api/routers/calculator_route.py
+++ calculator/src/api/routers/calculator_route.py
@@ -1,6 +1,7 @@
+import grpc
 from fastapi import APIRouter, Depends, HTTPException, status
-from pydantic import ValidationError
 
+from calculator.src.pb import add_pb2, add_pb2_grpc
 from calculator.src.validations.requests import AddRequest
 
 router = APIRouter()
@@ -14,11 +15,19 @@ router = APIRouter()
 def add_endpoint(
     query: AddRequest = Depends(),
 ):
-    try:
-        result = query.x + query.y
-        return result
-    except ValidationError as e:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=e.errors(),
-        )
+    # Připojení ke gRPC serveru pomocí hostname kontejneru (add)
+    with grpc.insecure_channel("add:50051") as channel:
+        stub = add_pb2_grpc.AddServiceStub(channel)
+
+        grpc_request = add_pb2.AddRequest(x=query.x, y=query.y)
+
+        try:
+            # Volání gRPC metody Add a získání odpovědi
+            response = stub.Add(grpc_request)
+        except grpc.RpcError as e:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
+            )
+
+    # Vracení výsledku jako odpověď FastAPI
+    return {"result": response.result}
